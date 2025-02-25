f6e730fdacaccb088d5e4e1a6703f461e14736b5
Pierre808 <pierre.equit@gmail.com>
2025-02-25 09:58:14 +0100

    added WorkspaceController.cs file

---
+++ NuvellAPI/Controllers/WorkspaceController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NuvellAPI.Interfaces;
+using NuvellAPI.Models.DTO;
+
+namespace NuvellAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WorkspaceController : ControllerBase
+{
+    
+}

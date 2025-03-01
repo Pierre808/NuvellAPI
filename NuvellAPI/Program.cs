246ed3b10d3b4ac34872e04536f4566bc77c4e92
Pierre808 <pierre.equit@gmail.com>
2025-03-01 17:11:19 +0100

    x

--- NuvellAPI/Program.cs
+++ NuvellAPI/Program.cs
@@ -9,7 +9,7 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. 
 if (builder.Environment.IsDevelopment())
 {
     builder.Configuration.AddUserSecrets<Program>();
